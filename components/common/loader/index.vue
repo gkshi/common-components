66b3abca8fa3d6d2195cefbc5da9bc162f28e069
George Shinkarev <gkshi@ya.ru>
2020-05-25 05:25:12 +0300

    [ADD] loader component

---
+++ components/common/loader/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .loader-component(v-if="localShow")
+    div Loading...
+</template>
+
+<script>
+export default {
+  name: 'loader-component',
+  props: {
+    show: Boolean
+  },
+  data () {
+    return {
+      localShow: this.show
+    }
+  },
+  watch: {
+    show () {
+      this.localShow = this.show
+    }
+  },
+  created () {
+    // Если не прокинули проп show, то делаем лоадер видимым вручную
+    if (!this.$options.propsData.show) {
+      this.localShow = true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .loader-component {
+    // your custom styles here
+  }
+</style>
--- plugins/common-components.js
+++ plugins/common-components.js
@@ -1,33 +1,36 @@
 import Vue from 'vue'
 
 import commonButton from '@/components/common/button'
-import commonInput from '@/components/common/input'
-import commonTextarea from '@/components/common/textarea'
-import commonFile from '@/components/common/file'
-import commonCounter from '@/components/common/counter'
-import commonRadio from '@/components/common/radio'
 import commonCheckbox from '@/components/common/checkbox'
+import commonCounter from '@/components/common/counter'
+import commonFile from '@/components/common/file'
+import commonInput from '@/components/common/input'
+import commonLoader from '@/components/common/loader'
 import commonModal from '@/components/common/modal'
+import commonRadio from '@/components/common/radio'
 import commonTabs from '@/components/common/tabs'
+import commonTextarea from '@/components/common/textarea'
 
 /* eslint-disable no-unused-vars */
 import commonButtonStyles from '@/components/common/button/_index.scss'
-import commonInputStyles from '@/components/common/input/_index.scss'
-import commonTextareaStyles from '@/components/common/textarea/_index.scss'
-import commonFileStyles from '@/components/common/file/_index.scss'
-import commonCounterStyles from '@/components/common/counter/_index.scss'
-import commonRadioStyles from '@/components/common/radio/_index.scss'
 import commonCheckboxStyles from '@/components/common/checkbox/_index.scss'
+import commonCounterStyles from '@/components/common/counter/_index.scss'
+import commonFileStyles from '@/components/common/file/_index.scss'
+import commonInputStyles from '@/components/common/input/_index.scss'
+import commonLoaderStyles from '@/components/common/loader/_index.scss'
 import commonModalStyles from '@/components/common/modal/_index.scss'
+import commonRadioStyles from '@/components/common/radio/_index.scss'
 import commonTabsStyles from '@/components/common/tabs/_index.scss'
+import commonTextareaStyles from '@/components/common/textarea/_index.scss'
 /* eslint-enable no-unused-vars */
 
 Vue.component('commonButton', commonButton)
-Vue.component('commonInput', commonInput)
-Vue.component('commonTextarea', commonTextarea)
-Vue.component('commonFile', commonFile)
-Vue.component('commonCounter', commonCounter)
-Vue.component('commonRadio', commonRadio)
 Vue.component('commonCheckbox', commonCheckbox)
+Vue.component('commonCounter', commonCounter)
+Vue.component('commonFile', commonFile)
+Vue.component('commonInput', commonInput)
+Vue.component('commonLoader', commonLoader)
 Vue.component('commonModal', commonModal)
+Vue.component('commonRadio', commonRadio)
 Vue.component('commonTabs', commonTabs)
+Vue.component('commonTextarea', commonTextarea)
